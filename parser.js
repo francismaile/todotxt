768639b0d5f99e556a32b4c78fccfd0524e1911d
Francis Maile <francismaile@gmail.com>
2018-02-20 16:47:54 -0800

    parser largely complete

--- parser.js
+++ parser.js
@@ -1,23 +1,25 @@
-// use replace
+function clog(...args) { console.log(...args) }
+
 // fix blank lines
+let todoList = [];
+
 function parse(theText) {
-	let todoList = [];
 	let id = 0;
 	const lines = theText.split("\n");
 	lines.forEach(function(line) {
-		let task = {};
-		task.id = id++; // should id start at 1?
-		let regResult;
+		let result;
 		let regex = new RegExp();
 		if( line !== '' ) {
+			let task = {};
+			task.id = ++id; // should id start at 1?
 			line = line.trim();
 			// if task is completed, the line will begin with a "x "
-			regex = /x\s/;
+			regex = /x\s+/;
 			if( result = regex.exec(line) ) {
 				line = line.slice(result[0].length);
 				task.completed = true;
 			}
-			// determine if marked with a priority. currently limited one of A, B , or C
+			// determine if marked with a priority. currently limited to one of A, B , or C
 			regex = /\((A|B|C)\)\s+/;
 			if( result = regex.exec(line) ) {
 				line = line.slice(result[0].length);
@@ -29,11 +31,11 @@ function parse(theText) {
 				// if second date, first is completion date and this is creation date
 				// else date is creation date
 			if( result = regex.exec(line) ) {
-				task.created = new Date(result[0].trim());
+				task.createdDate = new Date(result[0].trim());
 				line = line.slice(result[0].length);
 				if( result = regex.exec(line) ) {
-					task.completed = task.created;
-					task.created = new Date(result[0].trim());
+					task.completedDate = task.createdDate;
+					task.createdDate = new Date(result[0].trim());
 					line = line.slice(result[0].length);
 				}
 			}
@@ -47,44 +49,41 @@ function parse(theText) {
 				task.project = result[0].slice(1);
 				line = line.replace(regex, '');
 			}
-			// regex = /\w+:\w+/gi;
 			regex = /\w+:\d{4}-\d{1,2}-\d{1,2}|\w+:\w+/gi;
-			line.replace( regex, function(match) {
-				console.log(match);
-			});
-			if ( result = line.match(regex) ) {
-				let key, value;
-				task.tags = {};
-				result.forEach(function(tag) {
-					[key, value] = tag.split(":");
-					task.tags[key] = value;
-					line = line.replace(tag,'').trim();
-				});
-			}
-			task.description = line;
+			 if ( result = line.match(regex) ) {
+				 let key, value;
+				 task.tags = {};
+				 result.forEach(function(tag) {
+					 [key, value] = tag.split(":");
+					 task.tags[key] = value;
+					 line = line.replace(tag,'').trim();
+				 });
+			 }
+			task.description = line.trim();
 			line = "";
+			todoList.push(task);
 		}
-		todoList.push(task);
 	}); // end lines.forEach
-	return todoList;
+	// return todoList;
 } // end parse()
 
-// parse();
+function getFile(path) {
+	return fetch(path)
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (text) {
+			return text;
+    });
+}
 
-fetch("todo.txt") // Call the fetch function passing the url of the file
-	.then(function(response) {
-			console.log("fetching");
-			// Your code for handling the data you get from the API
-			let parsedList;
-			response.text().then( function(text) { 
-				parsedList = parse(text);
-				console.log(parsedList);
-			});
-			  		// do something with the text response 
-	})
-	.catch(function() {
-		// This is where you run code if the server returns any errors
-		//console.log("something went wrong");
-	});
+getAndParse("todo.txt", parse);
+
+function getAndParse(filePath, parseFctn) {
+	// get the file - returns a promise
+	const fileData = getFile(filePath);
+	// process result of promise
+	return fileData.then( data => parseFctn(data) );
 
 
+}

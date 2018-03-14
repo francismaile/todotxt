8baddb84311994c1037fb46c19b67e108f7b23cc
Francis Maile <francismaile@gmail.com>
2018-03-14 06:36:53 -0700

    most actions moved to separate functions. much easier to work with

--- display.js
+++ display.js
@@ -1,9 +1,11 @@
 function displayAllTodoLists() {
-	// displayList(category, which);
 	displayList('all', 'all');
 	displayList('project', 'all');
+	createCategoryMenu('project', 'all');
 	displayList('context', 'all');
+	createCategoryMenu('context', 'all');
 	displayList('priority', 'all');
+	createCategoryMenu('priority', 'all');
 	// displayList('tag', 'all');
 
 }
@@ -18,17 +20,6 @@ function sortByCategory( todoList, category ) {
 	});
 }
 
-/*
-
-completed:true
-context:"context"
-createdDate:"2018-02-09"
-description:"item no 1"
-priority:"A"
-project:"camelCaseTag"
-tags:due:"2018-02-13"
-key:"value"
-*/
 function displayTodoItem( taskId, task, listContainer ) {
 	let li = document.createElement('li');
 	li.id = 'task_' + taskId;
@@ -47,37 +38,47 @@ function insertDivider(category = 'Not Assigned', listContainer ) {
 	listContainer.appendChild(li);
 }
 
+function addMenuItem(category, currentCategory, menuElem) {
+	li = document.createElement('li');
+	li.className = '';
+	li.textContent =  currentCategory || 'Not Assigned';
+	li.onclick = function() { 
+		displayList(category, this.textContent);
+	};
+	menuElem.appendChild(li);
+}
+
+function createCategoryMenu( category ) {
+	let currentCategory = '';
+	const menuElem = document.getElementById(category + '-menu');
+	addMenuItem(category, 'All', menuElem );
+	for(var i=0; i < todoList.length; i++) {
+		if( category !== 'all' && currentCategory !== todoList[i][category] ) {
+			currentCategory = todoList[i][category];
+			addMenuItem(category, currentCategory, menuElem );
+		}
+	} // end for loop
+}
+
 function displayList(category = 'all', which = 'all') {
-	console.log('called with:', category, 'and', which);
+		console.log('called with:', category, 'and', which);
+	if( category === 'project' ) {
+		console.log(todoList[0]);
+	}
 	if( category !== 'all' ) {
-		todoList = sortByCategory( todoList, category );
+		let thisTodoList = sortByCategory( todoList, category );
 	}
 	const listContainer = document.getElementById(category + '-pane').firstElementChild;
 	listContainer.innerHTML = '';
 	// if which is 'all'
 	let currentCategory = '';
+
 	// insert item to list all of category. call; displayList(category, 'all');
 	for(var i=0; i < todoList.length; i++) {
-		if(i === 0) console.log(todoList[0]);
 		if( category !== 'all' && which === 'all' && currentCategory !== todoList[i][category] ) {
-			// sort todos by -category-
-			// insert divider with name of -category- before listing -category- todos
-			// add clickable name of project to nave menu pane
 			currentCategory = todoList[i][category];
 			insertDivider( currentCategory, listContainer );
-			const menuElem = document.getElementById(category + '-menu');
-			li = document.createElement('li');
-			li.className = '';
-			li.textContent =  currentCategory || 'Not Assigned';
-			li.onclick = function() { 
-				const whichOne = this.textContent;
-				displayList('project', whichOne);
-			};
-			menuElem.appendChild(li);
 		}
-		// called with: project and Health
-		// else which is a specific item in category
-		// display only todos in that item category
 		displayTodoItem( i, todoList[i], listContainer );
 	} // end for loop
 } // end displayList

e1da403d3ecc893a05aec43ff30a5e46c9b5d087
Francis Maile <francismaile@gmail.com>
2018-03-28 05:30:39 -0700

    complete rewrite of nav menu

--- display.js
+++ display.js
@@ -1,15 +1,14 @@
 function displayAllTodoLists() {
 	displayList('all', 'All');
-	displayList('project', 'All');
-	createCategoryMenu('project', 'All');
-	displayList('context', 'All');
-	createCategoryMenu('context', 'All');
-	displayList('priority', 'All');
-	createCategoryMenu('priority', 'All');
+	const projects = displayList('project', 'All');
+	const contexts = displayList('context', 'All');
+	const priorities = displayList('priority', 'All');
+	createMenu('project', projects);
+	createMenu('context', contexts);
+	createMenu('priority', priorities);
 }
 
 function createTodoItem( task ) {
-	// console.log(arguments);
 	listItem = document.createElement('li');
 	listItem.id = 'task_' + task.id;
 	listItem.textContent = task.description;
@@ -17,70 +16,77 @@ function createTodoItem( task ) {
 	return listItem;
 }
 
-function addMenuItem(category, currentCategory, menuElem) {
-	li = document.createElement('li');
-	li.className = '';
-	li.textContent =  currentCategory || 'Not Assigned';
-	li.onclick = function() { 
+function createMenuItem( category, item ) {
+	menuItem = document.createElement('li');
+	menuItem.textContent = item;
+	menuItem.className = 'menu-item';
+	menuItem.onclick = function() { 
 		displayList(category, this.textContent);
 	};
-	menuElem.appendChild(li);
+	return menuItem;
 }
 
-function createCategoryMenu( category ) {
-	let currentCategory = '';
-	const menuElem = document.getElementById(category + '-menu');
-	menuElem.innerHTML = '';
-	addMenuItem(category, 'All', menuElem );
-	for(var i=0; i < todoList.length; i++) {
-		if( category !== 'All' && currentCategory !== todoList[i][category] ) {
-			currentCategory = todoList[i][category];
-			addMenuItem(category, currentCategory, menuElem );
-		}
-	} // end for loop
+function createMenu( category, itemList ) {
+	const categoryMenu = document.getElementById(category + '-menu');
+	categoryMenu.appendChild(createMenuItem(category, 'All') );
+
+	itemList.forEach( item => 
+		categoryMenu.appendChild(createMenuItem(category, item) ) );
 }
 
 function displayList(category = 'all', which = 'All') {
-	// category = all, project, context or priority
+	// console.log('called with:', category, which);
 	let thisTodoList = [];
+	let categoryList = [];
 
 	const listContainer = document.getElementById(category + '-pane').firstElementChild;
 	listContainer.innerHTML = '';
 	let listItem;
 
 	if( category === 'all' ) {
-		thisTodoList = Array.from(todoList);
-		thisTodoList.forEach( function( todo ) {
+		todoList.forEach( function( todo ) {
 			listContainer.appendChild( createTodoItem(todo) );
 		});
 	} else {
+
 		thisTodoList = todoList.reduce(function ( taskList, task ) {
 			if( !( task[category] in taskList) ) taskList[task[category]] = [];
 			taskList[task[category]].push(task);
 			return taskList;
 		}, []);
 
-// display just one project/context/priority
-// undefined category needs to be pulled out until last and labeled:
-	// No Project/Context, Not Prioritized
-// priority needs to be sorted by priority letter
+		catList = Object.keys(thisTodoList);
+		catList.push(catList.splice(catList.indexOf('undefined'), 1)[0]);
+		if( category === 'priority' ) catList.sort();
 
-		Object.keys(thisTodoList).forEach( function ( element ) {
-			let categoryHeader = document.createElement('li');
-			categoryHeader.className = 'category-header';
-			let categoryTitle = document.createElement('h3');
-			categoryTitle.textContent = element;
-			categoryHeader.appendChild(categoryTitle);
-			let listBlock = document.createElement('ul');
-			listBlock.id = category + '-' + element.toLowerCase();
-			let listItem;
-			thisTodoList[element].forEach( function( todo ) {
-				listBlock.appendChild(createTodoItem(todo));
-				categoryHeader.appendChild(listBlock);
-			});
-			listContainer.appendChild(categoryHeader);
+		catList.forEach( function (taskCat, ind, arr) {
+			let categoryTitleText = '';
+			if(taskCat=== 'undefined' ) {
+				if( category === 'project' || category === 'context' )
+					categoryTitleText = 'No ' + category;
+				else if(category === 'priority')
+					categoryTitleText = 'Not prioritized';
+			} else {
+				categoryTitleText = taskCat;
+			}
+			categoryList.push(categoryTitleText);
+			if(  which === 'All' || which === taskCat || which === categoryTitleText) {
+				let categoryHeader = document.createElement('li');
+				categoryHeader.className = 'category-header';
+				let categoryTitle = document.createElement('h3');
+				categoryTitle.textContent = categoryTitleText;
+				categoryHeader.appendChild(categoryTitle);
+				let listBlock = document.createElement('ul');
+				listBlock.id = category + '-' + taskCat.toLowerCase();
+				let listItem;
+				thisTodoList[taskCat].forEach( function( todo ) {
+					listBlock.appendChild(createTodoItem(todo));
+					categoryHeader.appendChild(listBlock);
+				});
+				listContainer.appendChild(categoryHeader);
+			}
 		});
+		return categoryList; 
 	}
-
 } // end displayList
 

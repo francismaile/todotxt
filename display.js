e8087b34a92ed0c36f8742afa52b4967879a4475
Francis Maile <francismaile@gmail.com>
2018-04-05 14:11:49 -0700

    task item pretty much the way I want them

--- display.js
+++ display.js
@@ -8,15 +8,76 @@ function displayAllTodoLists() {
 	createMenu('priority', priorities);
 }
 
-function createTodoItem( task ) {
-	listItem = document.createElement('li');
-	listItem.id = 'task_' + task.id;
-	listItem.textContent = task.description;
+/*
+
+	<li id="task_0" class="task-item">
+			<input id="mark_complete_1" class="css-checkbox" type="checkbox" />
+			<label for="mark_complete_1" name="demo_lbl_2" class="css-label"></label>
+			<div style="display: inline-block;">learn to use command line commands</div>
+			<div id="todo-meta-taskid"><span class="task-meta task-meta-priority">(A)</span>
+			<span class="task-meta">@context</span></div>
+	</li>
+*/
+function createTodoItem( task, category ) {
+	const listItem = document.createElement('li');
 	listItem.className = 'task-item';
-	listItem.addEventListener( 'click', function( event ) {
+
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	checkbox.id = 'task_complete_' + task.id;
+	checkbox.className = "css-checkbox";
+
+	const label = document.createElement('label');
+	label.htmlFor = 'task_complete_' + task.id;
+	label.className = 'css-label';
+
+	listItem.appendChild(checkbox);
+	listItem.appendChild(label);
+
+	const div_description = document.createElement('div');
+	div_description.id = 'task_' + task.id;
+	div_description.className = 'task-description';
+	div_description.textContent = task.description;
+
+	listItem.appendChild(div_description);
+
+	const div_meta = document.createElement('div');
+
+	const span_priority = document.createElement('span');
+	span_priority.className = 'task-meta task-meta-priority';
+	if( task.priority && category !== 'priority' ) {
+		span_priority.textContent = `(${ task.priority })`;
+	}
+	div_meta.appendChild( span_priority );
+
+	let span_category;
+	if( task.context && category !== 'context' ) {
+		span_category = document.createElement('span');
+		span_category.className = 'task-meta';
+		span_category.textContent = `@${ task.context }`;
+		div_meta.appendChild( span_category );
+	}
+
+	if( task.project && category !== 'project' ) {
+		span_category = document.createElement('span');
+		span_category.className = 'task-meta';
+		span_category.textContent = `+${ task.project }`;
+		div_meta.appendChild( span_category );
+	}
+
+	div_description.addEventListener( 'click', function( event ) {
 		const taskid = event.target.id.split('_')[1];
 		editTask(taskid);
 	});
+
+	checkbox.addEventListener( 'click', function( event ) {
+		const taskid = event.target.id.split('_')[1];
+		markTaskComplete(taskid);
+	});
+
+
+	listItem.appendChild(div_meta);
+
 	return listItem;
 }
 
@@ -65,7 +126,7 @@ function displayList(category = 'all', which = 'All') {
 
 	if( category === 'all' ) {
 		todoList.forEach( function( todo ) {
-			listContainer.appendChild( createTodoItem(todo) );
+			listContainer.appendChild( createTodoItem(todo, category) );
 		});
 	} else {
 
@@ -100,7 +161,7 @@ function displayList(category = 'all', which = 'All') {
 				listBlock.id = category + '-' + taskCat.toLowerCase();
 				let listItem;
 				thisTodoList[taskCat].forEach( function( todo ) {
-					listBlock.appendChild(createTodoItem(todo));
+					listBlock.appendChild(createTodoItem(todo, category));
 					categoryHeader.appendChild(listBlock);
 				});
 				listContainer.appendChild(categoryHeader);

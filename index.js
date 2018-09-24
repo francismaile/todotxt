63da11d9e998fe7d81945eb7be56dd74ee74ac1e
Francis Maile <francismaile@gmail.com>
2018-09-23 18:19:07 -0700

    code cleanup

--- index.js
+++ index.js
@@ -123,3 +123,20 @@ countItems().then( function( cnt ) {
 function sortTodoList() {
 	
 }
+// choose local todo.txt file
+const fileSelector = document.createElement('input');
+fileSelector.setAttribute('type', 'file');
+
+const selectDialogueLink = document.createElement('a');
+selectDialogueLink.setAttribute('href', '');
+selectDialogueLink.innerText = "Select File";
+
+selectDialogueLink.onclick = function () {
+     fileSelector.click();
+     return false;
+}
+// document.body.appendChild(selectDialogueLink);
+     fileSelector.click(); // must be fired by user
+
+document.getElementById('downloadbutton').addEventListener('click', saveFile);
+
